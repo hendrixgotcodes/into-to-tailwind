fcf86d5eb7cf3f958cd3a982b16e2ab92471d981
Samuel O. Asare <asare11samuel@gmail.com>
2021-12-25 03:23:16 -1100

    feat(nabar.jsx & main.jsx): implemented tailwind flexbox
    Flexbox #5

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 export default function Main() {
     return (
         <main className='text-gray-600 px-16 py-6 font-body'>
-            <div>
+            <div className='flex justify-end'>
                 <a href="#" className='text-primary'>Login</a>
-                <a href="#">Sign up</a>
+                <a href="#" className='text-primary ml-2'>Sign up</a>
             </div>
+
+            
             <header>
                 <h2 className='text-gray-700 text-6xl font-semibold'>Recipes</h2>
                 <h3 className='text-2xl font-semibold' >For ninjas</h3>
@@ -24,9 +26,9 @@ export default function Main() {
                 </div>
 
                 <h4 className='font-bold mt-12 pb-2 border-b border-gray-200'>Most Popular</h4>
-                <div></div>
+                <div className='mt-8'></div>
 
-                <div className='mt-8'>
+                <div className='flex justify-center'>
                     <div className='bg-secondary-100 text-secondary-200'>Load more</div>
                 </div>
 
